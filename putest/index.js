5f1c73b39e14a587b20f27a531183d88129baa2e
ze.zhang <ze.zhang@11bee.com>
2020-01-15 11:23:11 +0800

    暂存

--- putest/index.js
+++ putest/index.js
@@ -1,3 +1,53 @@
+// const puppeteer = require('puppeteer');
+//
+// function test (){
+//     (async () => {
+//         const browser = await puppeteer.launch({
+//             // 无界面 默认为true,改成false,则可以看到浏览器操作，目前生成pdf只支持无界面的操作。
+//             headless: true,
+//             // 开启开发者调试模式，默认false, 也就是平时F12打开的面版
+//             devtools: true,
+//         });
+//         const page = await browser.newPage();
+//         await page.goto("https://www.huobi.io/zh-cn/");
+//         await page.waitForSelector('.btcusdt');
+//         const promise = new Promise((res, rej) => {setTimeout(() => {res()}, 2000)});
+//         await promise;
+//         const res = await page.evaluate(() => {
+//             let test =  document.querySelector(".btcusdt > .rate");
+//             return test.innerHTML;
+//         });
+//         console.log(res);
+//         await browser.close();
+//     })();
+// }
+//
+//
+// setInterval(test, 10000);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
 const puppeteer = require('puppeteer');
 
 function test (){
@@ -9,12 +59,12 @@ function test (){
             devtools: true,
         });
         const page = await browser.newPage();
-        await page.goto("https://www.huobi.io/zh-cn/");
-        await page.waitForSelector('.btcusdt');
+        await page.goto("http://quote.eastmoney.com/sh601901.html#fullScreenChart");
+        // await page.waitForSelector('.btcusdt');
         const promise = new Promise((res, rej) => {setTimeout(() => {res()}, 2000)});
         await promise;
         const res = await page.evaluate(() => {
-            let test =  document.querySelector(".btcusdt > .rate");
+            let test =  document.querySelector("#price9");
             return test.innerHTML;
         });
         console.log(res);
@@ -23,27 +73,4 @@ function test (){
 }
 
 
-setInterval(test, 10000)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+setInterval(test, 10000);
---
+++ socket.js
@@ -0,0 +1,127 @@
+// // 1 引入模块
+// const net = require('net');
+// // 2 创建服务器
+// let clientArr = [];
+// const server = net.createServer();
+// // 3 绑定链接事件
+// server.on('connection',(person)=>{
+//     console.log(clientArr.length);
+// // 记录链接的进程
+//     person.id = clientArr.length;
+//     clientArr.push(person);
+//     person.setEncoding('utf8');
+// // 客户socket进程绑定事件
+//     person.on('data',(chunk)=>{
+//         console.log(chunk);
+//
+//         clientArr.forEach((val)=>{
+// // 数据写入全部客户进程中
+//             val.write(chunk);
+//         })
+//     })
+//     person.on('close',(p1)=>{
+//         clientArr[p1.id] = null;
+//     } )
+//     person.on('error',(p1)=>{
+//         clientArr[p1.id] = null;
+//     })
+// })
+// server.listen(8080);
+
+
+/**
+ * Created by jinduo.quan on 2018/10/26.
+ */
+/**
+ * 参数：[socketOpen|socketClose|socketMessage|socketError] = func，[socket连接成功时触发|连接关闭|发送消息|连接错误]
+ * forever：连接关闭后是否重连
+ * maxConnect：最大重连次数，为always就一直重连
+ * timeout：连接超时时间
+ * @type {module.webSocket}
+ */
+module.exports =  class webSocket {
+    constructor(param = {}) {
+        this.param = param;
+        this.reconnectCount = 0;
+        this.socket = null;
+        this.forceClose = false;
+    }
+    connection = () => {
+        let {socketUrl, timeout = 0} = this.param;
+        this.socket = new WebSocket(socketUrl);
+        this.socket.onopen = this.onopen;
+        this.socket.onmessage = this.onmessage;
+        this.socket.onclose = this.onclose;
+        this.socket.onerror = this.onerror;
+        this.socket.sendMessage = this.sendMessage;
+        this.socket.closeSocket = this.closeSocket;
+        if(timeout) {
+            let time = setTimeout(() => {
+                if(this.socket && this.socket.readyState !== 1) {
+                    this.socket.close();
+                }
+                clearInterval(time);
+            }, timeout);
+        }
+    };
+    onopen = () => {
+        let {socketOpen} = this.param;
+        socketOpen && socketOpen();
+    };
+    onmessage = (msg) => {
+        let {socketMessage} = this.param;
+        socketMessage && socketMessage(msg);
+    };
+    onclose = (e) => {
+        let {socketClose, forever} = this.param;
+        if(forever) {
+            this.reconnection();
+        } else {
+            socketClose && socketClose({
+                status: 101,
+                msg: e
+            });
+        }
+    };
+    onerror = (e) => {
+        let {socketError} = this.param;
+        this.socket = null;
+        socketError && socketError(e);
+    };
+    sendMessage = (value) => {
+        if(this.socket) {
+            this.socket.send(JSON.stringify(value));
+        }
+    };
+    closeSocket = (forceClose = false) => {
+        if (this.socket) {
+            this.socket.close();
+            this.forceClose = forceClose;
+        }
+    };
+    reconnection = () => {
+        let {maxConnect = 0, socketClose} = this.param;
+        if(!this.forceClose && this.socket && (this.reconnectCount <= maxConnect || maxConnect === 'always') && this.socket.readyState === 3) {
+            this.reconnectCount += 1;
+            this.connection();
+        } else {
+            this.socket = null;
+            let errorMsg = {};
+            if(this.reconnectCount > maxConnect) {
+                errorMsg = {
+                    status: 101,
+                    msg: '已到达最大重连次数'
+                };
+            } else {
+                errorMsg = {
+                    status: 102,
+                    msg: '上个连接还未关闭'
+                };
+            }
+            socketClose && socketClose({
+                errorMsg
+            });
+        }
+    };
+};
+
