ed474f212b690a25952b1d5770f8c6e526dcf3c7
ze.zhang <ze.zhang@11bee.com>
2020-01-21 06:00:33 +0800

    暂存

--- index.js
+++ index.js
@@ -1,96 +1,23 @@
-
-//
-// console.warn(typeof(a));
-// var a = 3;
-// var a = function(){};
-
-
-
-
-// function getLastCount(){
-//     var thisHour = new Date().getHours();
-//     var thisMunite = new Date().getMinutes();
-//     var lastCount = 0;
-//     if(thisHour < 8){
-//         lastCount = 1000 - (1000 * (((60 * thisHour) + thisMunite) / (60 * 8)));
-//         return Math.round(lastCount);
-//     }
-//     if(thisHour < 20){
-//         lastCount = 1000 - (1000 * (((60 * (thisHour - 8)) + thisMunite) / (60 * 12)));
-//         return Math.round(lastCount);
-//     }
-//     if(thisHour < 24){
-//         lastCount = 500 - (500 * (((60 * (thisHour - 20)) + thisMunite) / (60 * 4)));
-//         return Math.round(lastCount);
-//     }
+// function* helloWorldGenerator() {
+//     console.log('Hello' + (yield)); // OK
+//     console.log('Hello' + (yield 123)); // OK
 // }
+//
+// var hw = helloWorldGenerator();
+//
+//
+// console.warn(hw.next())
+// console.warn(hw.next(true))
+// console.warn(hw.next())
+// console.warn(hw.next())
 
 
+let pro = new Promise((res, rej) => {
+    setTimeout(() => {res('test')}, 2000)
+});
 
-import ActionTypes from './utils/actionTypes'
-// 导出的就是一个方法 传入reducer方法 初始的state 以及增强器
-export default function createStore(reducer, preloadedState, enhancer) {
-    let currentReducer = reducer //当前的reducer函数
-    let currentState = preloadedState // 当前的页面的state
-    let currentListeners = [] // 当前的订阅事件数组 里面存的都是方法
-    let nextListeners = currentListeners // 用于更新的订阅数组
-    let isDispatching = false // 当前是否在dispatch执行中
-    // 其实就是浅克隆 nextListeners
-    function ensureCanMutateNextListeners() {
-        if (nextListeners === currentListeners) {
-            nextListeners = currentListeners.slice()
-        }
-    }
-    // 传说中的getState方法 其实就是把当前作用域内的currentState变量返回
-    // 注意不要直接改引用啊
-    function getState() {
-        return currentState
-    }
-    // 订阅方法 传入一个数组，并将其推入订阅数组内
-    function subscribe(listener) {
-        ensureCanMutateNextListeners()
-        nextListeners.push(listener)
-        // 返回一个方法 将该订阅方法从订阅数组中移除
-        // 这里不需要传入任何方法 利用闭包记住需要取消订阅的是哪个
-        return function unsubscribe() {
-            const index = nextListeners.indexOf(listener)
-            nextListeners.splice(index, 1)
-        }
-    }
-    // dispatch方法 传入一个action 将其传入reducer中得到新的state
-    function dispatch(action) {
-        try {
-            isDispatching = true
-            currentState = currentReducer(currentState, action)
-        } finally {
-            isDispatching = false
-        }
-        // 同时将订阅数组内的全部方法执行一遍
-        // 这里保证在订阅数组的方法执行的过程中 不会去更新reducer
-        const listeners = (currentListeners = nextListeners)
-        for (let i = 0; i < listeners.length; i++) {
-            const listener = listeners[i]
-            listener()
-        }
-
-        return action
-    }
-    // 替换reduer
-    function replaceReducer(nextReducer) {
-        currentReducer = nextReducer
-        dispatch({ type: ActionTypes.REPLACE })
-    }
-
-    // 创建的时候执行一个初始化的action
-    dispatch({ type: ActionTypes.INIT })
-    return {
-        dispatch,
-        subscribe,
-        getState,
-        replaceReducer,
-    }
-}
-
-
-
-
+pro.then((param) => {
+    console.warn(param);
+}).catch((e) => {
+    console.warn(e);
+});
--- putest/index.js
+++ putest/index.js
@@ -1,53 +1,3 @@
-// const puppeteer = require('puppeteer');
-//
-// function test (){
-//     (async () => {
-//         const browser = await puppeteer.launch({
-//             // 无界面 默认为true,改成false,则可以看到浏览器操作，目前生成pdf只支持无界面的操作。
-//             headless: true,
-//             // 开启开发者调试模式，默认false, 也就是平时F12打开的面版
-//             devtools: true,
-//         });
-//         const page = await browser.newPage();
-//         await page.goto("https://www.huobi.io/zh-cn/");
-//         await page.waitForSelector('.btcusdt');
-//         const promise = new Promise((res, rej) => {setTimeout(() => {res()}, 2000)});
-//         await promise;
-//         const res = await page.evaluate(() => {
-//             let test =  document.querySelector(".btcusdt > .rate");
-//             return test.innerHTML;
-//         });
-//         console.log(res);
-//         await browser.close();
-//     })();
-// }
-//
-//
-// setInterval(test, 10000);
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-
 const puppeteer = require('puppeteer');
 
 function test (){
@@ -59,12 +9,12 @@ function test (){
             devtools: true,
         });
         const page = await browser.newPage();
-        await page.goto("http://quote.eastmoney.com/sh601901.html#fullScreenChart");
-        // await page.waitForSelector('.btcusdt');
+        await page.goto("https://www.huobi.io/zh-cn/");
+        await page.waitForSelector('.btcusdt');
         const promise = new Promise((res, rej) => {setTimeout(() => {res()}, 2000)});
         await promise;
         const res = await page.evaluate(() => {
-            let test =  document.querySelector("#price9");
+            let test =  document.querySelector(".btcusdt > .rate");
             return test.innerHTML;
         });
         console.log(res);
@@ -74,3 +24,53 @@ function test (){
 
 
 setInterval(test, 10000);
+
+
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// const puppeteer = require('puppeteer');
+//
+// function test (){
+//     (async () => {
+//         const browser = await puppeteer.launch({
+//             // 无界面 默认为true,改成false,则可以看到浏览器操作，目前生成pdf只支持无界面的操作。
+//             headless: true,
+//             // 开启开发者调试模式，默认false, 也就是平时F12打开的面版
+//             devtools: true,
+//         });
+//         const page = await browser.newPage();
+//         await page.goto("http://quote.eastmoney.com/sh601901.html#fullScreenChart");
+//         // await page.waitForSelector('.btcusdt');
+//         const promise = new Promise((res, rej) => {setTimeout(() => {res()}, 2000)});
+//         await promise;
+//         const res = await page.evaluate(() => {
+//             let test =  document.querySelector("#price9");
+//             return test.innerHTML;
+//         });
+//         console.log(res);
+//         await browser.close();
+//     })();
+// }
+//
+//
+// setInterval(test, 10000);
