73e8c2891582cab422c8cf665b0c9e247844ac03
ze.zhang <ze.zhang@11bee.com>
2020-02-28 19:34:03 +0800

    项目

---
+++ emptyproject/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app";
+import "./styles.css";
+import "./styles.less";
+
+
+var mountNode = document.getElementById("app");
+ReactDOM.render(<App name="Jane" />, mountNode);
